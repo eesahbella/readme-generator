1d4a5353ac4ed4d61631a001767c6d1e6b743615
bella <isakawaii365@gmail.com>
2024-02-19 20:46:18 +0000

    adds function to display license badge

---
+++ generated-readme/README.md
@@ -0,0 +1,27 @@
+# fgfg 
+  ![Github license](https://img.shields.io/badge/license-MIT-blue)
+  ## Description
+  dfhbaefhb
+  ## Deployed Application URL
+  dgbdbg
+  ## Table of Contents
+  * [Description](#description)
+  * [Deployed Application URL](#DeployedApplicationURL)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Features](#features)
+  * [Contributing](#contributing)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  ## Installation
+  dgfhbdghb
+  ## Usage
+  dfhbbdgbn
+  ## Features
+  dgfhbdgnb
+  ## Contributing
+  fhbdgb
+  ## Testing
+  dgbhdgbndbg
+  ## Questions
+  Please send your questions [here](mailto:ghbdgbn) or visit [github/dfgbdgbn](https://github.com/dfgbdgbn).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,14 @@
+// Creating a function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue)`;
+  }
+  return "";
+}
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  ## Title
-  ${data.title}
+  return `# ${data.title} 
+  ${renderLicenseBadge(data.license)}
   ## Description
   ${data.description}
   ## Deployed Application URL
@@ -27,7 +33,9 @@ function generateMarkdown(data) {
   ## Testing
   ${data.test}
   ## Questions
-  Please send your questions [here](mailto:${data.email}) or visit [github/${data.creator}](https://github.com/${data.creator}).
+  Please send your questions [here](mailto:${data.email}) or visit [github/${
+    data.creator
+  }](https://github.com/${data.creator}).
 `;
 }
 
