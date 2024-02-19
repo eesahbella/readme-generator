fcd6c091c5ea5083705b4c7a9241b62176c754a2
bella <isakawaii365@gmail.com>
2024-02-19 19:43:22 +0000

    adds functions to index.js and generateMarkdown.js to generate readme file

--- index.js
+++ index.js
@@ -7,73 +7,89 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
   {
     type: "input",
-    name: "Title",
+    name: "title",
     message: "Give your project a title",
   },
   {
     type: "input",
-    name: "Description",
+    name: "description",
     message: "Describe the purpose of this project.",
   },
   {
     type: "input",
-    name: "Contents",
+    name: "contents",
     message: "Write your project's table of contents here.",
   },
   {
     type: "input",
-    name: "Installation",
+    name: "installation",
     message: "Describe the installation process for your project.",
   },
   {
     type: "input",
-    name: "Link",
-    message: "Provide URL to deployed application."
+    name: "link",
+    message: "Provide URL to deployed application.",
   },
   {
     type: "input",
-    name: "Usage",
+    name: "usage",
     message: "Describe how to use your project.",
   },
   {
     type: "input",
-    name: "Features",
+    name: "features",
     message: "List your project's key features here.",
   },
   {
     type: "checkbox",
-    name: "License",
+    name: "license",
     message: "Please select a license applicable to this project.",
-    choices: ["APACHE 2.0", "GPL v3", "MIT", "BSD2", "BSD3", "Boost 1.0", "MPL 2.0", "none"],
+    choices: [
+      "APACHE 2.0",
+      "GPL v3",
+      "MIT",
+      "BSD2",
+      "BSD3",
+      "Boost 1.0",
+      "MPL 2.0",
+      "none",
+    ],
   },
   {
     type: "input",
-    name: "Contributing",
+    name: "contributing",
     message: "List any contributors.",
     default: "",
   },
   {
     type: "input",
-    name: "Test",
+    name: "test",
     message: "If relevant, give a walkthrough of the necessary tests.",
   },
   {
     type: "input",
-    name: "Creator",
+    name: "creator",
     message: "Write your GitHub username.",
   },
   {
     type: "input",
-    name: "Email",
+    name: "email",
     message: "Provide a valid email address.",
   },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // function to initialize program
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Creating Professional README.md File...");
+    writeToFile("./generated-readme/README.md", generateMarkdown({ ...responses }));
+  });
+}
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,31 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## Description
+  ${data.description}
+  ## Deployed Application URL
+  ${data.link}
+  ## Table of Contents
+  * [Description](#description)
+  * [Deployed Application URL](#DeployedApplicationURL)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Features](#features)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Features
+  ${data.features}
+  ## Contributors
+  ${data.contributing}
+  ## Testing
+  ${data.test}
+  ## Questions
+  Please send your questions [here](mailto:${data.email}) or visit [github/${data.creator}](https://github.com/${data.creator}).
 `;
 }
 
